890d93ddacc4ba183a07449b54661dc02ebb7637
0xjosiah <josiah.webb@siliconmtn.com>
2023-05-25 17:16:31 -0600

    fills out int data create

--- lib/src/main/java/exercises/IntDataCreate.java
+++ lib/src/main/java/exercises/IntDataCreate.java
@@ -1,5 +1,20 @@
 package exercises;
 
 public class IntDataCreate {
-
+	// create a method that returns an int
+	/**
+	 * Calculates what the next score will need to be to reduce the average by 5%
+	 * @param scores Current scores
+	 * @return Score required to bring down the overall average by 5%
+	 */
+	public int createInt() {
+		int num = 5;
+		return num;
+	}
+	
+	public int printInt(int num) {
+		System.out.println(num);
+	}
+	
+	// create a method that prints the int to console
 }
