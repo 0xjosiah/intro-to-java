b59ab5e732d04cff0f5e19b9c58b9cdc1a46e0e1
0xjosiah <josiah.webb@siliconmtn.com>
2023-06-12 10:14:42 -0600

    creates test class for prim data class

---
+++ lib/src/test/java/exercises/PrimDataCreatorTest.java
@@ -0,0 +1,27 @@
+package exercises;
+
+/****************************************************************************
+ * <b>Title:</b> PrimDataCreatorTest.java
+ * <b>Project:</b> lib
+ * <b>Description:</b> test class for PrimDataCreator
+ * <b>Copyright:</b> Copyright (c) 2023
+ * <b>Company:</b> Silicon Mountain Technologies
+ * 
+ * @author Josiah Webb
+ * @version 3.x
+ * @since Jun 12, 2023
+ * <b>updates:</b>
+ *  
+ ****************************************************************************/
+
+public class PrimDataCreatorTest {
+	/**
+	 * test for PrimDataCreator
+	 * @param n/a
+	 * @return n/a
+	 */
+	public static void main(String[] args) {
+		PrimDataCreator data = new PrimDataCreator();
+		data.printDataTypes();		
+	}
+}
