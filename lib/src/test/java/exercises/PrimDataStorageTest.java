87cca299ef8242c6cf905bce7ee68606315bee3b
0xjosiah <josiah.webb@siliconmtn.com>
2023-06-12 10:15:24 -0600

    deletes unused test class

--- lib/src/test/java/exercises/PrimDataStorageTest.java
+++
@@ -1,9 +0,0 @@
-package exercises;
-
-public class PrimDataStorageTest {
-	
-	public static void main(String[] args) {
-		PrimDataCreator data = new PrimDataCreator();
-		data.printDataTypes();		
-	}
-}
