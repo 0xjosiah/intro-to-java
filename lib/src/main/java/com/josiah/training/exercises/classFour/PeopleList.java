0888f3e36670edeb1c55fbcd8e826950f518959b
0xjosiah <josiah.webb@siliconmtn.com>
2023-07-14 16:44:18 -0600

    creates three new classes for exercises from fourth class of dev training

---
+++ lib/src/main/java/com/josiah/training/exercises/classFour/PeopleList.java
@@ -0,0 +1,19 @@
+package com.josiah.training.exercises.classFour;
+
+/****************************************************************************
+ * <b>Title:</b> PeopleList.java
+ * <b>Project:</b> lib
+ * <b>Description:</b> exercise two from class four of dev training.
+ * <b>Copyright:</b> Copyright (c) 2023
+ * <b>Company:</b> Silicon Mountain Technologies
+ * 
+ * @author Josiah Webb
+ * @version 3.x
+ * @since Jul 14, 2023
+ * <b>updates:</b>
+ *  
+ ****************************************************************************/
+
+public class PeopleList {
+
+}
