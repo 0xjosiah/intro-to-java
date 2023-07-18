cae3aa05f0c26e7d4b07a675ebd557f0ca9994af
0xjosiah <josiah.webb@siliconmtn.com>
2023-07-18 16:09:23 -0600

    fills out person class

--- lib/src/main/java/com/josiah/training/exercises/classFour/Person.java
+++ lib/src/main/java/com/josiah/training/exercises/classFour/Person.java
@@ -3,7 +3,7 @@ package com.josiah.training.exercises.classFour;
 /****************************************************************************
  * <b>Title:</b> Person.java
  * <b>Project:</b> lib
- * <b>Description:</b> CHANGE ME!!
+ * <b>Description:</b> helper class used for PeopleList 
  * <b>Copyright:</b> Copyright (c) 2023
  * <b>Company:</b> Silicon Mountain Technologies
  * 
@@ -15,5 +15,43 @@ package com.josiah.training.exercises.classFour;
  ****************************************************************************/
 
 public class Person {
-
+	private int id;
+	private String firstName;
+	private String lastName;
+	
+	/**
+	 * constructor 
+	 * @param id
+	 * @param first
+	 * @param last
+	 */
+	public Person(int id, String first, String last) {
+		this.id = id;
+		this.firstName = first;
+		this.lastName = last;
+	}
+	
+	/**
+	 * get person id
+	 * @return id
+	 */
+	public int getPersonId() {
+		return this.id;
+	}
+	
+	/**
+	 * get person's first name
+	 * @return firstName
+	 */
+	public String getFirstName() {
+		return this.firstName;
+	}
+	
+	/**
+	 * get person's last name
+	 * @return firstName
+	 */
+	public String getLastName() {
+		return this.lastName;
+	}
 }
