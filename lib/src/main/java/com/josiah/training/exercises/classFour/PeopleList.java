83ec5e54b1aa96ebdefb8af4d9619c4c12b9009b
0xjosiah <josiah.webb@siliconmtn.com>
2023-07-18 16:38:48 -0600

    removes unused import

--- lib/src/main/java/com/josiah/training/exercises/classFour/PeopleList.java
+++ lib/src/main/java/com/josiah/training/exercises/classFour/PeopleList.java
@@ -1,7 +1,6 @@
 package com.josiah.training.exercises.classFour;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 
 /****************************************************************************
