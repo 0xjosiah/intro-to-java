011e2f09d25832e337300a55179fb9b8645b42b6
0xjosiah <josiah.webb@siliconmtn.com>
2023-07-18 15:53:30 -0600

    refactors variables

--- lib/src/main/java/com/josiah/training/exercises/classFour/StatesMap.java
+++ lib/src/main/java/com/josiah/training/exercises/classFour/StatesMap.java
@@ -50,16 +50,16 @@ public class StatesMap {
 		// init temp to store sorted map
 		HashMap<String, String> shuffled = new HashMap<String, String>();
 		// create list to get keys from existing map
-		ArrayList<String> keys = new ArrayList<String>(mapOfStates.keySet());
+		ArrayList<String> keys = new ArrayList<String>(this.mapOfStates.keySet());
 		// shuffle keys from map
 		Collections.shuffle(keys);
 		// loop thru keys
 		for(Object stateKey : keys) {
 			// put key-value pairs into temp map
-			shuffled.put((String)stateKey, mapOfStates.get(stateKey));
+			shuffled.put((String)stateKey, this.mapOfStates.get(stateKey));
 		}
 		// cast new map into existing mapOfStates, cast to ensure correct type
-		return (HashMap<String, String>) (mapOfStates = shuffled);
+		return (HashMap<String, String>) (this.mapOfStates = shuffled);
 	}
 	
 	/**
@@ -74,56 +74,56 @@ public class StatesMap {
 	 * method loads map of states
 	 */
 	protected void loadMap() {
-		mapOfStates.put("AL", "Alabama");
-		mapOfStates.put("AK", "Alaska");
-		mapOfStates.put("AZ", "Arizona");
-		mapOfStates.put("AR", "Arkansas");
-		mapOfStates.put("CA", "California");
-		mapOfStates.put("CO", "Colorado");
-		mapOfStates.put("CT", "Connecticut");
-		mapOfStates.put("DE", "Delaware");
-		mapOfStates.put("FL", "Florida");
-		mapOfStates.put("GA", "Georgia");
-		mapOfStates.put("HI", "Hawaii");
-		mapOfStates.put("ID", "Idaho");
-		mapOfStates.put("IL", "Illinois");
-		mapOfStates.put("IN", "Indiana");
-		mapOfStates.put("IA", "Iowa");
-		mapOfStates.put("KS", "Kansas");
-		mapOfStates.put("KY", "Kentucky");
-		mapOfStates.put("LA", "Louisiana");
-		mapOfStates.put("ME", "Maine");
-		mapOfStates.put("MD", "Maryland");
-		mapOfStates.put("MA", "Massachusetts");
-		mapOfStates.put("MI", "Michigan");
-		mapOfStates.put("MN", "Minnesota");
-		mapOfStates.put("MN", "Mississippi");
-		mapOfStates.put("MO", "Missouri");
-		mapOfStates.put("MT", "Montana");
-		mapOfStates.put("NE", "Nebraska");
-		mapOfStates.put("NV", "Nevada");
-		mapOfStates.put("NH", "New Hampshire");
-		mapOfStates.put("NJ", "New Jersey");
-		mapOfStates.put("NM", "New Mexico");
-		mapOfStates.put("NY", "New York");
-		mapOfStates.put("NC", "North Carolina");
-		mapOfStates.put("ND", "North Dakota");
-		mapOfStates.put("OH", "Ohio");
-		mapOfStates.put("OK", "Oklahoma");
-		mapOfStates.put("OR", "Oregon");
-		mapOfStates.put("PA", "Pennsylvania");
-		mapOfStates.put("RI", "Rhode Island");
-		mapOfStates.put("SC", "South Carolina");
-		mapOfStates.put("SD", "South Dakota");
-		mapOfStates.put("TN", "Tennessee");
-		mapOfStates.put("TX", "Texas");
-		mapOfStates.put("UT", "Utah");
-		mapOfStates.put("VT", "Vermont");
-		mapOfStates.put("VA", "Virginia");
-		mapOfStates.put("WA", "Washington");
-		mapOfStates.put("WV", "West Virginia");
-		mapOfStates.put("WI", "Wisconsin");
-		mapOfStates.put("WY", "Wyoming");
+		this.mapOfStates.put("AL", "Alabama");
+		this.mapOfStates.put("AK", "Alaska");
+		this.mapOfStates.put("AZ", "Arizona");
+		this.mapOfStates.put("AR", "Arkansas");
+		this.mapOfStates.put("CA", "California");
+		this.mapOfStates.put("CO", "Colorado");
+		this.mapOfStates.put("CT", "Connecticut");
+		this.mapOfStates.put("DE", "Delaware");
+		this.mapOfStates.put("FL", "Florida");
+		this.mapOfStates.put("GA", "Georgia");
+		this.mapOfStates.put("HI", "Hawaii");
+		this.mapOfStates.put("ID", "Idaho");
+		this.mapOfStates.put("IL", "Illinois");
+		this.mapOfStates.put("IN", "Indiana");
+		this.mapOfStates.put("IA", "Iowa");
+		this.mapOfStates.put("KS", "Kansas");
+		this.mapOfStates.put("KY", "Kentucky");
+		this.mapOfStates.put("LA", "Louisiana");
+		this.mapOfStates.put("ME", "Maine");
+		this.mapOfStates.put("MD", "Maryland");
+		this.mapOfStates.put("MA", "Massachusetts");
+		this.mapOfStates.put("MI", "Michigan");
+		this.mapOfStates.put("MN", "Minnesota");
+		this.mapOfStates.put("MN", "Mississippi");
+		this.mapOfStates.put("MO", "Missouri");
+		this.mapOfStates.put("MT", "Montana");
+		this.mapOfStates.put("NE", "Nebraska");
+		this.mapOfStates.put("NV", "Nevada");
+		this.mapOfStates.put("NH", "New Hampshire");
+		this.mapOfStates.put("NJ", "New Jersey");
+		this.mapOfStates.put("NM", "New Mexico");
+		this.mapOfStates.put("NY", "New York");
+		this.mapOfStates.put("NC", "North Carolina");
+		this.mapOfStates.put("ND", "North Dakota");
+		this.mapOfStates.put("OH", "Ohio");
+		this.mapOfStates.put("OK", "Oklahoma");
+		this.mapOfStates.put("OR", "Oregon");
+		this.mapOfStates.put("PA", "Pennsylvania");
+		this.mapOfStates.put("RI", "Rhode Island");
+		this.mapOfStates.put("SC", "South Carolina");
+		this.mapOfStates.put("SD", "South Dakota");
+		this.mapOfStates.put("TN", "Tennessee");
+		this.mapOfStates.put("TX", "Texas");
+		this.mapOfStates.put("UT", "Utah");
+		this.mapOfStates.put("VT", "Vermont");
+		this.mapOfStates.put("VA", "Virginia");
+		this.mapOfStates.put("WA", "Washington");
+		this.mapOfStates.put("WV", "West Virginia");
+		this.mapOfStates.put("WI", "Wisconsin");
+		this.mapOfStates.put("WY", "Wyoming");
 	}
 	
 	/**
