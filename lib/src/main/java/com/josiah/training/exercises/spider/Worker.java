d996f4fb84ce6befd2a9b9cd30fd7119ea39fee6
0xjosiah <josiah.webb@siliconmtn.com>
2023-09-14 14:19:08 -0600

    creates spider package and placeholder worker class

---
+++ lib/src/main/java/com/josiah/training/exercises/spider/Worker.java
@@ -0,0 +1,21 @@
+package com.josiah.training.exercises.spider;
+
+/****************************************************************************
+ * <b>Title:</b> Worker.java
+ * <b>Project:</b> lib
+ * <b>Description:</b> CHANGE ME!!
+ * <b>Copyright:</b> Copyright (c) 2023
+ * <b>Company:</b> Silicon Mountain Technologies
+ * 
+ * @author Josiah Webb
+ * @version 3.x
+ * @since Sep 14, 2023
+ * <b>updates:</b>
+ *  
+ ****************************************************************************/
+
+public class Worker {
+	public static void main(String[] args) {
+		
+	}
+}
