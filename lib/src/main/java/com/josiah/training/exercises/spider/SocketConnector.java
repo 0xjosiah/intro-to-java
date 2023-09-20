0d77ce9e397ee21b440cb92b7ae597ef514962d8
0xjosiah <josiah.webb@siliconmtn.com>
2023-09-20 14:30:31 -0600

    copies over socket class as start point

---
+++ lib/src/main/java/com/josiah/training/exercises/spider/SocketConnector.java
@@ -0,0 +1,114 @@
+package com.josiah.training.exercises.spider;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+/****************************************************************************
+ * <b>Title:</b> SocketConnector.java
+ * <b>Project:</b> lib
+ * <b>Description:</b> CHANGE ME!!
+ * <b>Copyright:</b> Copyright (c) 2023
+ * <b>Company:</b> Silicon Mountain Technologies
+ * 
+ * @author Josiah Webb
+ * @version 3.x
+ * @since Sep 20, 2023
+ * <b>updates:</b>
+ *  
+ ****************************************************************************/
+
+public class SocketConnector {
+	// initialized private variables for constructor
+	private String host = null;
+	private int portNumber = 0;
+	// static path to be used for testing
+	private static final String PATH = "/home/josiah/dev/intro-to-java/lib/src/main/java/com/josiah/training/exercises/serverConnector/";
+	
+	
+	/**
+	 * constructor
+	 * @param host
+	 * @param portNumber
+	 */
+	public SocketConnector(String host, int portNumber) {
+		this.host = host;
+		this.portNumber = portNumber;
+	}
+	
+	/**
+	 * reaches out to a server socket, connects, and extracts html from page
+	 * @param host
+	 * @param portNumber
+	 * @return
+	 * @throws IOException
+	 */
+	public String getWebPage() throws IOException{
+		// init string builder for ingested html
+		StringBuilder html = new StringBuilder();
+		
+		// try socket connection
+		try (
+				Socket echoSocket = new Socket(this.host, this.portNumber);
+				DataOutputStream out = new DataOutputStream(echoSocket.getOutputStream());
+				BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()))
+			) {
+			out.writeBytes("GET / HTTPS/1.1\r\nHost: " + host + "\r\n\r\n");
+			String inData = null;
+			while((inData = in.readLine()) != null ) {
+				html.append(inData);
+			}
+		} 
+		
+		catch (IOException e) {
+		    System.out.println("Unable to connect to server: " + e.getMessage());
+		}
+		
+		return html.toString();
+	}
+	
+	
+	/**
+	 * prints fetched content
+	 * @param content
+	 */
+	public void printPageToConsole(String content) {
+		System.out.println(content);
+	}
+	
+	/**
+	 * takes in content, a destination file, and copies content to file
+	 * @param content
+	 * @param outputFile
+	 */
+	public void fileWriter(String content, String rootFolder, String outputFile) {
+		String outputPath = rootFolder + File.separator + outputFile;
+		// init output file
+		File output = new File(outputPath);
+		
+		try {
+			FileWriter writer = new FileWriter(output);
+			writer.write(content);
+			writer.close();
+		} catch(IOException e) {
+			System.out.println(e);
+		}
+	}
+	
+	/**
+	 * calls constructor and tests with localhost default page
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+		SocketConnector fetcher = new SocketConnector("smt-stage.qa.siliconmtn.com", 443);
+		String content = fetcher.getWebPage();
+		fetcher.printPageToConsole(content);
+		fetcher.fileWriter(content, PATH, "output.txt");
+	}
+
+}
